e022317ef043dae48a2030e00735c8b74fd04949
YuHo-chang <changhoward.cs07@nctu.edu.tw>
2019-12-27 22:49:00 +0800

    done

---
+++ 7-2 City Powering.cpp
@@ -0,0 +1,110 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+class city
+{
+	public:
+		city(long long A, long long X, long long Y)
+		{
+			a = A;
+			x = X;
+			y = Y;
+		}
+		city()
+		{
+			a = -1;
+			x = y = -1;
+		}
+		long long a, x, y;
+	private:
+};
+
+
+long long MST( vector<vector<long long>> &con, int n)
+{
+	
+	vector<bool> v(n + 1, false);
+	vector<long long> disto_root(n + 1, 10000000000);
+	disto_root[0] = 0;
+
+	for (int i = 1; i <= n; i++) 
+	{
+		int vertex_now = -1;
+		long long min_cost = 10000000000;
+
+		for (int j = 0; j <= n; j++)
+		{
+			if (!v[j] && disto_root[j] < min_cost)
+			{
+				min_cost = disto_root[j];
+				vertex_now = j;
+			}
+		}
+
+		if (vertex_now == -1)
+			break;
+
+		v[vertex_now] = true;
+
+		for (int vertex_next = 0; vertex_next <= n; vertex_next++)
+		{
+			if (!v[vertex_next] && con[vertex_now][vertex_next] < disto_root[vertex_next])
+				disto_root[vertex_next] = con[vertex_now][vertex_next];
+		}
+	}
+	
+	long long cost = 0;
+
+	for (int i = 0; i <= n; i++)
+	{
+		cost += disto_root[i];
+	}
+	return cost;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<city> c(n+1);
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> c[i].a;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> c[i].x >> c[i].y;
+	}
+	
+	vector < vector<long long>> connection(n + 1, vector<long long>(n + 1, -1));
+
+	for (int i = 1; i <= n; i++)
+	{
+		connection[i][0] = c[i].a;
+		connection[0][i] = c[i].a;
+	}
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+		{
+			if (i == j)
+			{
+				connection[i][j] = c[i].a;
+				continue;
+			}
+			connection[i][j] = abs(c[i].x - c[j].x) + abs(c[i].y - c[j].y);
+		}
+	
+
+	cout << MST(connection, n);
+
+	return 0;
+}
+
---
+++ 7-2 Increasing path.cpp
@@ -0,0 +1,137 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+void IPath(long long s, long long t, long long n, vector<long long> &h, vector<bool> &v, vector<long long> &p, vector<vector<long long>> &g)
+{
+	queue<long long> q;
+	
+	q.push(s);
+	v[s] = true;
+	p[s] = s;
+	//h[s] = 0;
+
+	while (!q.empty())
+	{
+		long long temp = q.front();
+		q.pop();
+
+
+		for (long long i = 0; i < n; i++)
+		{
+			if (!v[i] && g[temp][i] >= h[temp] + 1)
+			{
+				v[i] = true;
+				p[i] = temp;
+				h[i] = h[temp] + 1;
+				q.push(i);
+				if (i == t)
+				{
+					return;
+				}
+			}
+		}
+	}
+	return;
+}
+
+void BFS(long long s, long long t, long long n, vector<long long> &h, vector<bool> &v, vector<vector<long long>> &g)
+{
+	queue<long long> q;
+
+	q.push(s);
+	v[s] = true;
+	//p[s] = s;
+	//h[s] = 0;
+
+	while (!q.empty())
+	{
+		long long temp = q.front();
+		q.pop();
+
+		for (long long i = 0; i < n; i++)
+		{
+			if (!v[i] && g[temp][i]>0)
+			{
+				v[i] = true;
+				//p[i] = temp;
+				h[i] = h[temp] + 1;
+				q.push(i);
+				if (i == t)
+				{
+					return;
+				}
+			}
+		}
+	}
+	return;
+}
+
+void traverse(vector<long long> &p, long t)
+{
+	if (p[t] == t)
+	{
+		cout << t << " ";
+		return;
+	}
+	traverse(p, p[t]);
+	cout << t << " ";
+	return;
+}
+
+int main()
+{
+	long long T;
+	cin >> T;
+
+	for (long long i = 0; i < T; i++)
+	{
+		long long n, m, s, t;
+		cin >> n >> m >> s >> t;
+		vector<vector<long long>> graph(n, vector<long long> (n ,-1));
+		for (long long j = 0; j < m; j++)
+		{
+			long long u, v, w;
+			cin >> u >> v >> w;
+			graph[u][v] = max(graph[u][v], w);
+		}
+		if (s == t)
+		{
+			cout << t << "\n";
+			continue;
+		}
+		vector<long long> height(n, 0);
+		vector<bool> v(n, false);
+		vector<long long> p(n, -1);
+		
+		vector<long long> hBFS(n, 0);
+		vector<bool> vBFS(n, false);
+		//vector<long long> pBFS(n, 0);
+		
+		IPath(s, t, n, height, v, p, graph);
+		BFS(s, t, n, hBFS, vBFS, graph);
+		
+		if (height[t] == hBFS[t])
+		{
+			if (height[t] > 0)
+			{
+				traverse(p, t);
+				cout << "\n";
+			}
+			else
+				cout << -1 << "\n";
+		}
+		else
+		{
+			cout << -1 << "\n";
+		}
+		
+	}
+
+	return 0;
+}
+
